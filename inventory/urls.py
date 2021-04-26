4e6507a18adda875f77482a147ef108608ecc48a
NMunro <neilmunro@gmail.com>
2021-04-26 23:31:07 +0100

    #27 Added initial login templates and urls

--- inventory/urls.py
+++ inventory/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("product/search/", views.ProductSearchView.as_view(), name="search"),
+    path("", views.ProductSearchView.as_view(), name="search"),
     path("product/<int:product_id>/", views.ProductView.as_view(), name="product"),
 ]
